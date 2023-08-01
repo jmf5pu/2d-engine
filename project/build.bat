5e01d9094f71f755ed3e9e85d3b633a8ee2e574f
Justin <jmf5pu@virginia.edu>
2023-07-31 21:11:59 -0400

    tutorial p3 completion

--- project/build.bat
+++ project/build.bat
@@ -1,4 +1,6 @@
-set files=src\glad.c src\main.c src\engine\global.c src\engine\render\render.c src\engine\render\render_init.c
+set render=src\engine\render\render.c src\engine\render\render_init.c src\engine\render\render_util.c
+set io=src\engine\io\io.c
+set files=src\glad.c src\main.c src\engine\global.c %render% %io%
 set libs=..\lib\SDL2main.lib ..\lib\SDL2.lib ..\lib\freetype.lib
 
 CL /Zi /I ..\include %files% /link %libs% /OUT:mygame.exe
