f7d635f01acc4098008f351427c1139e93f138ad
jmf5pu <jmf5pu@virginia.edu>
2023-12-10 20:53:02 -0500

    add files and function signatures

---
+++ project/src/menu_helpers/menu_helpers.c
@@ -0,0 +1,17 @@
+#include "menu_helpers.h"
+
+void render_main_menu(void){
+    return;
+}
+
+void render_game_mode_menu(void){
+    return;
+}
+
+void render_map_select_menu(void){
+    return;
+}
+
+void render_pause_menu(void){
+    return;
+}
---
+++ project/src/menu_helpers/menu_helpers.h
@@ -0,0 +1,9 @@
+#ifndef MAP_HELPERS_H
+#define MAP_HELPERS_H
+
+void render_main_menu(void);
+void render_game_mode_menu(void);
+void render_map_select_menu(void);
+void render_pause_menu(void);
+
+#endif
