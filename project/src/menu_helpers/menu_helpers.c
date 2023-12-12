2bc91eeae32630912ca0c151d0ed91ae31930852
jmf5pu <jmf5pu@virginia.edu>
2023-12-11 21:33:59 -0500

    add letter animations & hashmap

--- project/src/menu_helpers/menu_helpers.c
+++ project/src/menu_helpers/menu_helpers.c
@@ -1,5 +1,276 @@
 #include "menu_helpers.h"
 
+void init_letter_hashmap(void){
+    letter_anim_map = create_hash_map(LETTER_ANIM_COUNT);
+    insert(letter_anim_map, "A", anim_A);
+    insert(letter_anim_map, "B", anim_B);
+    insert(letter_anim_map, "C", anim_C);
+    insert(letter_anim_map, "D", anim_D);
+    insert(letter_anim_map, "E", anim_E);
+    insert(letter_anim_map, "F", anim_F);
+    insert(letter_anim_map, "G", anim_G);
+    insert(letter_anim_map, "H", anim_H);
+    insert(letter_anim_map, "I", anim_I);
+    insert(letter_anim_map, "J", anim_J);
+    insert(letter_anim_map, "K", anim_K);
+    insert(letter_anim_map, "L", anim_L);
+    insert(letter_anim_map, "M", anim_M);
+    insert(letter_anim_map, "N", anim_N);
+    insert(letter_anim_map, "O", anim_O);
+    insert(letter_anim_map, "P", anim_P);
+    insert(letter_anim_map, "Q", anim_Q);
+    insert(letter_anim_map, "R", anim_R);
+    insert(letter_anim_map, "S", anim_S);
+    insert(letter_anim_map, "T", anim_T);
+    insert(letter_anim_map, "U", anim_U);
+    insert(letter_anim_map, "V", anim_V);
+    insert(letter_anim_map, "W", anim_W);
+    insert(letter_anim_map, "X", anim_X);
+    insert(letter_anim_map, "Y", anim_Y);
+    insert(letter_anim_map, "Z", anim_Z);
+}
+
+void init_letter_anims(void){
+    render_sprite_sheet_init(&sprite_sheet_A, "assets/letters/A.png", 40, 50);
+    adef_A = animation_definition_create(
+        &sprite_sheet_A,
+        (f32[]){0},
+        (u8[]){0},
+        (u8[]){0},
+        1);
+    anim_A = animation_create(adef_A, false);
+
+    render_sprite_sheet_init(&sprite_sheet_B, "assets/letters/B.png", 40, 50);
+    adef_B = animation_definition_create(
+        &sprite_sheet_B,
+        (f32[]){0},
+        (u8[]){0},
+        (u8[]){0},
+        1);
+    anim_B = animation_create(adef_B, false);
+
+    render_sprite_sheet_init(&sprite_sheet_C, "assets/letters/C.png", 40, 50);
+    adef_C = animation_definition_create(
+        &sprite_sheet_C,
+        (f32[]){0},
+        (u8[]){0},
+        (u8[]){0},
+        1);
+    anim_C = animation_create(adef_C, false);
+
+    render_sprite_sheet_init(&sprite_sheet_D, "assets/letters/D.png", 40, 50);
+    adef_D = animation_definition_create(
+        &sprite_sheet_D,
+        (f32[]){0},
+        (u8[]){0},
+        (u8[]){0},
+        1);
+    anim_D = animation_create(adef_D, false);
+
+    render_sprite_sheet_init(&sprite_sheet_E, "assets/letters/E.png", 40, 50);
+    adef_E = animation_definition_create(
+        &sprite_sheet_E,
+        (f32[]){0},
+        (u8[]){0},
+        (u8[]){0},
+        1);
+    anim_E = animation_create(adef_E, false);
+
+    render_sprite_sheet_init(&sprite_sheet_F, "assets/letters/F.png", 40, 50);
+    adef_F = animation_definition_create(
+        &sprite_sheet_F,
+        (f32[]){0},
+        (u8[]){0},
+        (u8[]){0},
+        1);
+    anim_F = animation_create(adef_F, false);
+
+    render_sprite_sheet_init(&sprite_sheet_G, "assets/letters/G.png", 40, 50);
+    adef_G = animation_definition_create(
+        &sprite_sheet_G,
+        (f32[]){0},
+        (u8[]){0},
+        (u8[]){0},
+        1);
+    anim_G = animation_create(adef_G, false);
+
+    render_sprite_sheet_init(&sprite_sheet_H, "assets/letters/H.png", 40, 50);
+    adef_H = animation_definition_create(
+        &sprite_sheet_H,
+        (f32[]){0},
+        (u8[]){0},
+        (u8[]){0},
+        1);
+    anim_H = animation_create(adef_H, false);
+
+    render_sprite_sheet_init(&sprite_sheet_I, "assets/letters/I.png", 40, 50);
+    adef_I = animation_definition_create(
+        &sprite_sheet_I,
+        (f32[]){0},
+        (u8[]){0},
+        (u8[]){0},
+        1);
+    anim_I = animation_create(adef_I, false);
+
+    render_sprite_sheet_init(&sprite_sheet_J, "assets/letters/J.png", 40, 50);
+    adef_J = animation_definition_create(
+        &sprite_sheet_J,
+        (f32[]){0},
+        (u8[]){0},
+        (u8[]){0},
+        1);
+    anim_J = animation_create(adef_J, false);
+
+    render_sprite_sheet_init(&sprite_sheet_K, "assets/letters/K.png", 40, 50);
+    adef_K = animation_definition_create(
+        &sprite_sheet_K,
+        (f32[]){0},
+        (u8[]){0},
+        (u8[]){0},
+        1);
+    anim_K = animation_create(adef_K, false);
+
+    render_sprite_sheet_init(&sprite_sheet_L, "assets/letters/L.png", 40, 50);
+    adef_L = animation_definition_create(
+        &sprite_sheet_L,
+        (f32[]){0},
+        (u8[]){0},
+        (u8[]){0},
+        1);
+    anim_L = animation_create(adef_L, false);
+
+    render_sprite_sheet_init(&sprite_sheet_M, "assets/letters/M.png", 40, 50);
+    adef_M = animation_definition_create(
+        &sprite_sheet_M,
+        (f32[]){0},
+        (u8[]){0},
+        (u8[]){0},
+        1);
+    anim_M = animation_create(adef_M, false);
+
+    render_sprite_sheet_init(&sprite_sheet_N, "assets/letters/N.png", 40, 50);
+    adef_N = animation_definition_create(
+        &sprite_sheet_N,
+        (f32[]){0},
+        (u8[]){0},
+        (u8[]){0},
+        1);
+    anim_N = animation_create(adef_N, false);
+
+    render_sprite_sheet_init(&sprite_sheet_O, "assets/letters/O.png", 40, 50);
+    adef_O = animation_definition_create(
+        &sprite_sheet_O,
+        (f32[]){0},
+        (u8[]){0},
+        (u8[]){0},
+        1);
+    anim_O = animation_create(adef_O, false);
+
+    render_sprite_sheet_init(&sprite_sheet_P, "assets/letters/P.png", 40, 50);
+    adef_P = animation_definition_create(
+        &sprite_sheet_P,
+        (f32[]){0},
+        (u8[]){0},
+        (u8[]){0},
+        1);
+    anim_P = animation_create(adef_P, false);
+
+    render_sprite_sheet_init(&sprite_sheet_Q, "assets/letters/Q.png", 40, 50);
+    adef_Q = animation_definition_create(
+        &sprite_sheet_Q,
+        (f32[]){0},
+        (u8[]){0},
+        (u8[]){0},
+        1);
+    anim_Q = animation_create(adef_Q, false);
+
+    render_sprite_sheet_init(&sprite_sheet_R, "assets/letters/R.png", 40, 50);
+    adef_R = animation_definition_create(
+        &sprite_sheet_R,
+        (f32[]){0},
+        (u8[]){0},
+        (u8[]){0},
+        1);
+    anim_R = animation_create(adef_R, false);
+
+    render_sprite_sheet_init(&sprite_sheet_S, "assets/letters/S.png", 40, 50);
+    adef_S = animation_definition_create(
+        &sprite_sheet_S,
+        (f32[]){0},
+        (u8[]){0},
+        (u8[]){0},
+        1);
+    anim_S = animation_create(adef_S, false);
+
+    render_sprite_sheet_init(&sprite_sheet_T, "assets/letters/T.png", 40, 50);
+    adef_T = animation_definition_create(
+        &sprite_sheet_T,
+        (f32[]){0},
+        (u8[]){0},
+        (u8[]){0},
+        1);
+    anim_T = animation_create(adef_T, false);
+    render_sprite_sheet_init(&sprite_sheet_U, "assets/letters/U.png", 40, 50);
+    adef_U = animation_definition_create(
+        &sprite_sheet_U,
+        (f32[]){0},
+        (u8[]){0},
+        (u8[]){0},
+        1);
+    anim_U = animation_create(adef_U, false);
+
+    render_sprite_sheet_init(&sprite_sheet_V, "assets/letters/V.png", 40, 50);
+    adef_V = animation_definition_create(
+        &sprite_sheet_V,
+        (f32[]){0},
+        (u8[]){0},
+        (u8[]){0},
+        1);
+    anim_V = animation_create(adef_V, false);
+
+    render_sprite_sheet_init(&sprite_sheet_W, "assets/letters/W.png", 40, 50);
+    adef_W = animation_definition_create(
+        &sprite_sheet_W,
+        (f32[]){0},
+        (u8[]){0},
+        (u8[]){0},
+        1);
+    anim_W = animation_create(adef_W, false);
+
+    render_sprite_sheet_init(&sprite_sheet_X, "assets/letters/X.png", 40, 50);
+    adef_X = animation_definition_create(
+        &sprite_sheet_X,
+        (f32[]){0},
+        (u8[]){0},
+        (u8[]){0},
+        1);
+    anim_X = animation_create(adef_X, false);
+
+    render_sprite_sheet_init(&sprite_sheet_Y, "assets/letters/Y.png", 40, 50);
+    adef_Y = animation_definition_create(
+        &sprite_sheet_Y,
+        (f32[]){0},
+        (u8[]){0},
+        (u8[]){0},
+        1);
+    anim_Y = animation_create(adef_Y, false);
+
+    render_sprite_sheet_init(&sprite_sheet_Z, "assets/letters/Z.png", 40, 50);
+    adef_Z = animation_definition_create(
+        &sprite_sheet_Z,
+        (f32[]){0},
+        (u8[]){0},
+        (u8[]){0},
+        1);
+    anim_Z = animation_create(adef_Z, false);
+}
+
+// initializes all menu-related assets at game start up
+void init_menus(void){
+    init_letter_anims();
+    init_letter_hashmap();
+}
+
 void render_main_menu(void){
     return;
 }
--- project/src/menu_helpers/menu_helpers.h
+++ project/src/menu_helpers/menu_helpers.h
@@ -1,6 +1,96 @@
 #ifndef MAP_HELPERS_H
 #define MAP_HELPERS_H
 
+#define LETTER_ANIM_COUNT 26
+
+#include "../engine/render.h"
+#include "../engine/hash_map.h"
+#include "../engine/animation.h"
+
+Hash_Map * letter_anim_map;
+
+Sprite_Sheet sprite_sheet_A;
+Sprite_Sheet sprite_sheet_B;
+Sprite_Sheet sprite_sheet_C;
+Sprite_Sheet sprite_sheet_D;
+Sprite_Sheet sprite_sheet_E;
+Sprite_Sheet sprite_sheet_F;
+Sprite_Sheet sprite_sheet_G;
+Sprite_Sheet sprite_sheet_H;
+Sprite_Sheet sprite_sheet_I;
+Sprite_Sheet sprite_sheet_J;
+Sprite_Sheet sprite_sheet_K;
+Sprite_Sheet sprite_sheet_L;
+Sprite_Sheet sprite_sheet_M;
+Sprite_Sheet sprite_sheet_N;
+Sprite_Sheet sprite_sheet_O;
+Sprite_Sheet sprite_sheet_P;
+Sprite_Sheet sprite_sheet_Q;
+Sprite_Sheet sprite_sheet_R;
+Sprite_Sheet sprite_sheet_S;
+Sprite_Sheet sprite_sheet_T;
+Sprite_Sheet sprite_sheet_U;
+Sprite_Sheet sprite_sheet_V;
+Sprite_Sheet sprite_sheet_W;
+Sprite_Sheet sprite_sheet_X;
+Sprite_Sheet sprite_sheet_Y;
+Sprite_Sheet sprite_sheet_Z;
+
+Animation_Definition * adef_A;
+Animation_Definition * adef_B;
+Animation_Definition * adef_C;
+Animation_Definition * adef_D;
+Animation_Definition * adef_E;
+Animation_Definition * adef_F;
+Animation_Definition * adef_G;
+Animation_Definition * adef_H;
+Animation_Definition * adef_I;
+Animation_Definition * adef_J;
+Animation_Definition * adef_K;
+Animation_Definition * adef_L;
+Animation_Definition * adef_M;
+Animation_Definition * adef_N;
+Animation_Definition * adef_O;
+Animation_Definition * adef_P;
+Animation_Definition * adef_Q;
+Animation_Definition * adef_R;
+Animation_Definition * adef_S;
+Animation_Definition * adef_T;
+Animation_Definition * adef_U;
+Animation_Definition * adef_V;
+Animation_Definition * adef_W;
+Animation_Definition * adef_X;
+Animation_Definition * adef_Y;
+Animation_Definition * adef_Z;
+
+Animation * anim_A;
+Animation * anim_B;
+Animation * anim_C;
+Animation * anim_D;
+Animation * anim_E;
+Animation * anim_F;
+Animation * anim_G;
+Animation * anim_H;
+Animation * anim_I;
+Animation * anim_J;
+Animation * anim_K;
+Animation * anim_L;
+Animation * anim_M;
+Animation * anim_N;
+Animation * anim_O;
+Animation * anim_P;
+Animation * anim_Q;
+Animation * anim_R;
+Animation * anim_S;
+Animation * anim_T;
+Animation * anim_U;
+Animation * anim_V;
+Animation * anim_W;
+Animation * anim_X;
+Animation * anim_Y;
+Animation * anim_Z;
+
+void init_menus(void);
 void render_main_menu(void);
 void render_game_mode_menu(void);
 void render_map_select_menu(void);
