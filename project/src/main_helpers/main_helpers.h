38867e4c8b898c470f895f0b333cbb02deab0646
jmf5pu <jmf5pu@virginia.edu>
2024-03-08 17:08:57 -0500

    update header file

--- project/src/main_helpers/main_helpers.h
+++ project/src/main_helpers/main_helpers.h
@@ -2,9 +2,13 @@
 #define MAIN_HELPERS_H
 
 #include "../structs.h"
+#include "../player_helpers/player_structs.h"
 #include <stdbool.h>
+#include <linmath.h>
+#include <SDL2/SDL.h>
 
 bool should_destroy_entity(Entity *entity, Map *map);
 bool entity_is_player_or_crosshair(Entity *entity);
+void render_player_anims(Player * player, SDL_Window *window, u32 texture_slots[32], vec4 color);
 
 #endif
