558c3c8e14329a777a8643753b1e67738ce0567e
jmf5pu <jmf5pu@virginia.edu>
2024-07-03 07:29:23 -0400

    add more sprites, fix some logic

--- project/src/hud/hud.h
+++ project/src/hud/hud.h
@@ -18,53 +18,11 @@
 Hash_Map *ammo_anim_map;
 
 // health display sprite sheets, animation definitions, animations
-Sprite_Sheet sprite_sheet_health_cross_0;
-Sprite_Sheet sprite_sheet_health_cross_1;
-Sprite_Sheet sprite_sheet_health_cross_2;
-Sprite_Sheet sprite_sheet_health_cross_3;
-Sprite_Sheet sprite_sheet_health_cross_4;
-Sprite_Sheet sprite_sheet_health_cross_5;
-Sprite_Sheet sprite_sheet_health_cross_6;
-Sprite_Sheet sprite_sheet_health_cross_7;
-Sprite_Sheet sprite_sheet_health_cross_8;
-Sprite_Sheet sprite_sheet_health_cross_9;
-Sprite_Sheet sprite_sheet_health_cross_10;
-Sprite_Sheet sprite_sheet_health_cross_11;
-Sprite_Sheet sprite_sheet_health_cross_12;
-Sprite_Sheet sprite_sheet_health_cross_13;
-Sprite_Sheet sprite_sheet_health_cross_14;
+Sprite_Sheet sprite_sheet_health_meter_0;
 
-Animation_Definition *adef_health_cross_0;
-Animation_Definition *adef_health_cross_1;
-Animation_Definition *adef_health_cross_2;
-Animation_Definition *adef_health_cross_3;
-Animation_Definition *adef_health_cross_4;
-Animation_Definition *adef_health_cross_5;
-Animation_Definition *adef_health_cross_6;
-Animation_Definition *adef_health_cross_7;
-Animation_Definition *adef_health_cross_8;
-Animation_Definition *adef_health_cross_9;
-Animation_Definition *adef_health_cross_10;
-Animation_Definition *adef_health_cross_11;
-Animation_Definition *adef_health_cross_12;
-Animation_Definition *adef_health_cross_13;
-Animation_Definition *adef_health_cross_14;
+Animation_Definition *adef_health_meter_0;
 
-Animation *anim_health_cross_0;
-Animation *anim_health_cross_1;
-Animation *anim_health_cross_2;
-Animation *anim_health_cross_3;
-Animation *anim_health_cross_4;
-Animation *anim_health_cross_5;
-Animation *anim_health_cross_6;
-Animation *anim_health_cross_7;
-Animation *anim_health_cross_8;
-Animation *anim_health_cross_9;
-Animation *anim_health_cross_10;
-Animation *anim_health_cross_11;
-Animation *anim_health_cross_12;
-Animation *anim_health_cross_13;
-Animation *anim_health_cross_14;
+Animation *anim_health_meter_0;
 
 // ammo display sprite sheets, animation definitions, animations
 Sprite_Sheet sprite_sheet_ammo_0;
@@ -149,11 +107,13 @@ extern HUD *hud;
 
 void init_hud(SDL_Window *window);
 void render_hud(void);
-void render_health(SDL_Window *window, u32 texture_slots[BATCH_SIZE], Player *player, vec2 position, vec4 color);
+void render_health(Player *player, vec2 position);
 void render_ammo(SDL_Window *window, u32 texture_slots[BATCH_SIZE], Player *player, vec2 position, vec4 color);
 void fix_crosshair_position(Player *player);
 void free_hud(void);
-
+void init_ammo_anim_hashmap(void);
+void init_health_anims(void);
+void init_ammo_anims(void);
 void init_interact_bar_adefs(void);
 void render_interact_bar_progress(Entity *entity, f32 percentage);
 
